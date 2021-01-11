4799f3c303003f27eb30d2fe5414634e04e1c82d
EL93019205 <el93019205_2@yahoo.co.jp>
2021-01-11 21:08:17 +0900

    MySQLを扱おう

--- main.py
+++ main.py
@@ -1,40 +1,29 @@
 """
 main
 """
-import sqlite3
+# pylint: disable=E0401
+import mysql.connector
 
 
-# conn = sqlite3.connect('test_sqlite.db')
-conn = sqlite3.connect(':memory:')
+conn = mysql.connector.connect(host='127.0.0.1')
+cursor = conn.cursor()
+cursor.execute('CREATE DATABASE test_mysql_database')
 
-curs = conn.cursor()
-
-curs.execute(
-    'CREATE TABLE persons(id INTEGER PRIMARY KEY AUTOINCREMENT, name STRING )')
+cursor = conn.cursor()
+cursor.execute('CREATE TABLE persons('
+               'id int NOT NULL AUTO_INCREMENT,'
+               'name varchar(14) NOT NULL,'
+               'PRIMARY KEY(id))')
+cursor.execute('INSERT INTO persons(name) values("Mike")')
 conn.commit()
 
-curs.execute(
-    'INSERT INTO persons(name) values("Mike")'
-)
+cursor.execute('SELECT * FROM persons')
+for row in cursor:
+    print(row)
+
+cursor.execute('UPDATE persons set name = "Michael" WHERE name = "Mike"')
+cursor.execute('DELETE FROM persons WHERE name = "Michael"')
+
 conn.commit()
-
-
-curs.execute(
-    'INSERT INTO persons(name) values("Nancy")'
-)
-curs.execute(
-    'INSERT INTO persons(name) values("Jun")'
-)
-conn.commit()
-
-curs.execute('UPDATE persons set name = "Michael" WHERE name = "Mike"')
-conn.commit()
-
-curs.execute('DELETE FROM persons WHERE name = "Michael"')
-conn.commit()
-
-curs.execute('SELECT * FROM persons')
-print(curs.fetchall())
-
-curs.close()
+cursor.close()
 conn.close()
